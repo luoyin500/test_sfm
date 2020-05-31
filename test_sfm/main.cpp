c15a6517842fc76439c3370d7b54b31e12ec1f07
luoyin500 <804612149@qq.com>
2020-05-31 20:43:42 +0800

    change

--- test_sfm/main.cpp
+++ test_sfm/main.cpp
@@ -1,11 +1,55 @@
+#include <opencv2\xfeatures2d\nonfree.hpp>
+#include <opencv2\features2d\features2d.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <opencv2\calib3d\calib3d.hpp>
 
 #include <iostream>
 
-int main()
+using namespace cv;
+using namespace std;
+
+void extract_features(
+	vector<string>& image_names,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Mat>& descriptor_for_all,
+	vector<vector<Vec3b>>& colors_for_all
+)
 {
 
+	key_points_for_all.clear();
+	descriptor_for_all.clear();
+	Mat image;
+
+	//读取图像，获取图像特征点，并保存
+	Ptr<Feature2D> sift = xfeatures2d::SIFT::create(0, 3, 0.04, 10);
+	for (auto it = image_names.begin(); it != image_names.end(); ++it)
+	{
+		image = imread(*it);
+		if (image.empty()) continue;
+
+		vector<KeyPoint> key_points;
+		Mat descriptor;
+		//偶尔出现内存分配失败的错误
+		sift->detectAndCompute(image, noArray(), key_points, descriptor);
+
+		//特征点过少，则排除该图像
+		if (key_points.size() <= 10) continue;
+
+		key_points_for_all.push_back(key_points);
+		descriptor_for_all.push_back(descriptor);
+
+		vector<Vec3b> colors(key_points.size());
+		for (int i = 0; i < key_points.size(); ++i)
+		{
+			Point2f& p = key_points[i].pt;
+			colors[i] = image.at<Vec3b>(p.y, p.x);
+		}
+		colors_for_all.push_back(colors);
+	}
+}
+
+int main()
+{
 
 
 	return 0;
